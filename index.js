d62d86dc4dc4e70aa48d2315b6b5d00cf6777ac1
jaz1492 <88153708+jaz1492@users.noreply.github.com>
2024-03-13 16:15:08 +0900

    Rework files for winner and rename files

--- index.js
+++ index.js
@@ -10,23 +10,43 @@ update scoreboard
 const form = document.querySelector("form"); 
 const selection = ["rock","paper","scissors"];
 // determines if the player is the winner using numbers from html file as well as a random num for computer.
-// It returns a boolean as the result.
+// It returns a string as the result and updates the score.
 const isPlayerWinner = function(player,computer){
     switch(player){
         case 0:
-            if(computer == 0){console.log("draw")}
-            else if (computer == 2){return true}
-            else{return false}
-        break;
-        case 1: if(computer == 1){console.log("draw")}
-        else if (computer == 0){return true}
-        else{return false}
-        break;
-        case 2: if(computer == 1){console.log("draw")}
-        else if (computer == 1){return true}
-        else{return false}
-        break;
+            if(computer == 0){return announceResults("Draw")}
+            else if (computer == 2){
+                updateScore("player");
+                return announceResults("Winner")}
+            else{
+                updateScore("computer");
+                return announceResults("Loser")}
+            break;
+        case 1: 
+            if(computer == 1){return announceResults("Draw")}
+            else if (computer == 0){
+                updateScore("player");
+                return announceResults("Winner")}
+            else{
+                updateScore("computer");
+                return announceResults("Loser")}
+            break;
+        case 2: 
+            if(computer == 2){return announceResults("Draw")}
+            else if (computer == 1){
+                updateScore("player");
+                return announceResults("Winner")}
+            else{
+                updateScore("computer");
+                return announceResults("Loser")}
+            break;
     }
+}
+const announceResults = function(result){
+    const popup = document.querySelector(".pop-up");
+    popup.classList.remove('hide');
+    popup.innerHTML = result;
+    setTimeout(()=>popup.classList.add('hide'),1000) 
 }
 //function to update the score of the winner. 
 const updateScore = function(winner){
@@ -36,6 +56,6 @@ const updateScore = function(winner){
 form.addEventListener("submit", (event)=>{
     const playersChoice = parseInt(event.srcElement.elements["player-choice"].value); // grabs clients choice.
     const computerChoice = Math.floor(Math.random()*3); // function to generate comp choice.
-    console.log(isPlayerWinner(playersChoice,computerChoice),`player:${selection[playersChoice]}, computer:${selection[computerChoice]}`);
+    isPlayerWinner(playersChoice,computerChoice);
     event.preventDefault();//this stops the form from refreshing the page. I plan to change this once forms are covered. 
 })
