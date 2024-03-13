c0d4dd6f956ba244b3de662ab927c2d2963979de
jaz1492 <88153708+jaz1492@users.noreply.github.com>
2024-03-13 14:57:38 +0900

    Add function to determine winner

--- index.js
+++ index.js
@@ -8,10 +8,29 @@ Give results
 update scoreboard
 */
 const form = document.querySelector("form"); 
-const computerChoice = ()=>Math.floor(Math.random()*3); // function to generate comp choice.
-
+const selection = ["rock","paper","scissors"];
+// determines if the player is the winner using numbers from html file as well as a random num for computer.
+// It returns a boolean as the result.
+const isPlayerWinner = function(player,computer){
+    switch(player){
+        case 0:
+            if(computer == 0){console.log("draw")}
+            else if (computer == 2){return true}
+            else{return false}
+        break;
+        case 1: if(computer == 1){console.log("draw")}
+        else if (computer == 0){return true}
+        else{return false}
+        break;
+        case 2: if(computer == 1){console.log("draw")}
+        else if (computer == 1){return true}
+        else{return false}
+        break;
+    }
+}
 form.addEventListener("submit", (event)=>{
-    let playersChoice = parseInt(event.srcElement.elements["player-choice"].value); // grabs clients choice.
-    
+    const playersChoice = parseInt(event.srcElement.elements["player-choice"].value); // grabs clients choice.
+    const computerChoice = Math.floor(Math.random()*3); // function to generate comp choice.
+    console.log(isPlayerWinner(playersChoice,computerChoice),`player:${selection[playersChoice]}, computer:${selection[computerChoice]}`);
     event.preventDefault();//this stops the form from refreshing the page. I plan to change this once forms are covered. 
 })
