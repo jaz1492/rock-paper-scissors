d5fd8a660ce0f6004ba70135914b0af9c0274e9d
jaz1492 <88153708+jaz1492@users.noreply.github.com>
2024-03-13 14:36:23 +0900

    Create pseudocode and basic variables

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const form = document.querySelector("form");
-const selection = ["rock","paper","scissors"]
+/*
+what to do:
+Grab an input from client.
+Generate a choice for the computer.
+Compare results 
+Give results
+    If results are same, alert and go back to input
+update scoreboard
+*/
+const form = document.querySelector("form"); 
+const computerChoice = ()=>Math.floor(Math.random()*3); // function to generate comp choice.
+
 form.addEventListener("submit", (event)=>{
-    let playersChoice = event.srcElement.elements["player-choice"].value;
+    let playersChoice = parseInt(event.srcElement.elements["player-choice"].value); // grabs clients choice.
+    
     event.preventDefault();//this stops the form from refreshing the page. I plan to change this once forms are covered. 
 })
